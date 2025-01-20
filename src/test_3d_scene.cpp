06cf4e55f30934daf879c01262f2249778accd43
Thiago Amendola <amendolathiago@gmail.com>
2025-01-20 00:44:43 -0800

    Add missing code for animated bg

--- src/test_3d_scene.cpp
+++ src/test_3d_scene.cpp
@@ -2,6 +2,7 @@
 #include "test_3d_scene_statics.h"
 
 #include "bn_bg_palettes_actions.h"
+#include "bn_regular_bg_animate_actions.h"
 #include "bn_colors.h"
 #include "bn_core.h"
 #include "bn_fixed.h"
@@ -19,17 +20,21 @@
 #include "scene_type.h"
 #include "stage_section_renderer.h"
 
-#include "bn_sprite_items_butano_background_2.h"
+// #include "bn_sprite_items_butano_background_2.h"
 #include "bn_sprite_items_ninja.h"
 #include "models/shot.h"
 
 #include "bn_regular_bg_items_floor.h"
-#include "bn_regular_bg_items_moon.h"
+// #include "bn_regular_bg_items_moon.h"
+#include "bn_regular_bg_items_bg_anim.h"
 #include "bn_regular_bg_ptr.h"
 
 test_3d_scene::test_3d_scene()
     : _player_ship(&_controller, &_camera, &_models), _prepare_to_leave(false),
-      _scene_bg(bn::regular_bg_items::floor.create_bg(0, 0))
+      _anim_bg(bn::regular_bg_items::bg_anim.create_bg(0, 0)),
+      _anim_bg_action(bn::create_regular_bg_cached_animate_action_forever(
+            _anim_bg, 6, bn::regular_bg_items::bg_anim.map_item(), 0, 1, 2, 3))
+    //   _scene_bg(bn::regular_bg_items::floor.create_bg(0, 0)),
 //   _moon_bg(bn::regular_bg_items::moon.create_bg(0, 20))
 //   _test_sprite_sprite_3d_item(bn::sprite_items::butano_background_2, 0)
 {
@@ -37,7 +42,8 @@ test_3d_scene::test_3d_scene()
     _camera.set_position(fr::point_3d(0, 1040, 0));
     _models.load_colors(scene_colors, get_scene_color_mapping());
 
-    bn::bg_palettes::set_transparent_color(bn::color(25, 18, 25));
+    // Scenario setup
+    // bn::bg_palettes::set_transparent_color(bn::color(25, 18, 25));
 
     // Set music
     // bn::music_items::gameplay.play(1); // Very slow
@@ -53,6 +59,9 @@ bn::optional<scene_type> test_3d_scene::update()
 {
     bn::optional<scene_type> result;
     int static_count = 0; 
+
+    // <-- move
+    _anim_bg_action.update();
 
     if (_prepare_to_leave)
     {
